0417326228d6bff6a9dfb83d66b9961f14dfe4cd
Nathan Burns <nathanlburns@gmail.com>
2011-06-03 09:10:06 -0700

    Updated uuid_mysql.rb to be compatible with mysql2

--- lib/usesguid/uuid_mysql.rb
+++ lib/usesguid/uuid_mysql.rb
@@ -13,11 +13,17 @@ class UUID
   # We'll retrieve a bunch of guids at a time to reduce the # of DB round-trips.
   # If the guid bucket is empty, re-fill it by calling MySQL.  Then return a guid.
   def UUID.mysql_create(connection=ActiveRecord::Base.connection)
-    raise "UUID.mysql_create only works with MySQL" unless connection.adapter_name.downcase =~ /mysql/
+    adapter_name = connection.adapter_name.downcase
+    raise "UUID.mysql_create only works with MySQL" unless adapter_name =~ /mysql/
     @@guid_bucket_mutex.synchronize do
       if @@guid_bucket.blank?
         uuid_functions = Array.new(BUCKET_SIZE, "UUID()")
-        @@guid_bucket = connection.execute("SELECT #{uuid_functions.join(',')}").fetch_row
+        # Mysql2Adapter doesn't support fetch_row.
+        if adapter_name == "mysql2"
+          @@guid_bucket = connection.select_rows("SELECT #{uuid_functions.join(',')}").first
+        else
+          @@guid_bucket = connection.execute("SELECT #{uuid_functions.join(',')}").fetch_row
+        end
       end
       # My tests show shift is much faster than slice!(0), pop, or delete_at(0) 
       parse @@guid_bucket.shift
