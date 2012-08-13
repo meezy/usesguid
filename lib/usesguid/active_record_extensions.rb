5f1596b1ca1809fca5d25ac7bd9deb0148724ebe
terrbear <theath@gmail.com>
2012-08-13 13:31:12 -0500

    more heavy handed guid assignment

--- lib/usesguid/active_record_extensions.rb
+++ lib/usesguid/active_record_extensions.rb
@@ -11,24 +11,24 @@
 
 module Usesguid
   module ActiveRecordExtensions
-    
+
     #def self.append_features( base )
     def self.included( base )
       super
       base.extend( ClassMethods )
 
       base.class_eval do
-        before_create :assign_guid
+        before_save :assign_guid
       end
-      
+
       base.primary_key = 'id'
     end
 
     def assign_guid
-      if self[self.class.primary_key].blank? 
+      if self[self.class.primary_key].blank? || self.new_record?
         self[self.class.primary_key] = UUID.mysql_create(self.connection).to_s
       end
-    end    
-    
+    end
+
   end #ActiveRecordExtensions
 end #Usesguid
